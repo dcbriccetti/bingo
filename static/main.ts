3f5039767204fdfedae91a3ab8f6f9e55cf02a52
Dave Briccetti <daveb@davebsoft.com>
2021-05-03 09:59:13 -0700

    Improve code quality

--- static/main.ts
+++ static/main.ts
@@ -1,21 +1,19 @@
 class Bingo {
-    private eventSource: EventSource
+    private readonly eventSource: EventSource
     private readonly playerCardCells: NodeListOf<HTMLTableDataCellElement>
-    private NUMBER_OF_LETTERS = 5
-    private NUMBERS_PER_LETTER = 15
+    private readonly NUMBER_OF_LETTERS = 5
+    private readonly NUMBERS_PER_LETTER = 15
 
     constructor() {
         this.eventSource = new EventSource('/events')
         this.playerCardCells = document.querySelectorAll('#player-card td')
 
-        this.populatePlayerCard()
-
-        for (let i = 1; i <= 75; i++)
-            this.cellFor(i).textContent = i.toString()
+        this.createReceivedDisplay()
+        this.createPlayerCard()
 
         this.eventSource.onmessage = event => {
             const ballNumber: number = parseInt(event.data)
-            this.cellFor(ballNumber).classList.add('received')
+            this.receivedDisplayCellFor(ballNumber).classList.add('received')
             if (this.updatePlayerCard(ballNumber)) {
                 this.eventSource.close()
             }
@@ -28,15 +26,31 @@ class Bingo {
 
     }
 
-    private cellFor(oneBasedNumber: number): HTMLTableDataCellElement {
-        const zeroBasedNumber = oneBasedNumber - 1
+    /** Sets the numbers 1 to 75, 15 per column B to O, on the received display */
+    private createReceivedDisplay() {
+        for (let i = 1; i <= 75; i++)
+            this.receivedDisplayCellFor(i).textContent = i.toString()
+    }
+
+    /**
+     * Returns the HTMLTableDataCellElement for the ball number given
+     * @param ballNumber a number from 1 to 75
+     */
+    private receivedDisplayCellFor(ballNumber: number): HTMLTableDataCellElement {
+        const zeroBasedNumber = ballNumber - 1
         const colIndex = Math.floor(zeroBasedNumber / this.NUMBERS_PER_LETTER)
         const rowIndex = zeroBasedNumber % this.NUMBERS_PER_LETTER
         const receivedCells: NodeListOf<HTMLTableDataCellElement> = document.querySelectorAll('#balls-received td')
         return receivedCells[rowIndex * this.NUMBER_OF_LETTERS + colIndex]
     }
 
-    private populatePlayerCard(): void {
+    /** Puts randomly selected numbers, in the right range, in each of the 5 B…O columns */
+    private createPlayerCard(): void {
+
+        /**
+         * Returns an array of five numbers randomly selected from the 15 integers starting with lowerBound
+         * @param lowerBound the lower number in the range of 15 numbers
+         */
         function selectFiveNumbers(lowerBound: number): number[] {
             const fifteenNumbers: number[] = Array.from({length: 15}, (_, i: number) => i + lowerBound) // 1–15, or 16–29, etc.
             return Array.from({length: 5}, () => {  // Choose 5 randomly
@@ -47,30 +61,36 @@ class Bingo {
             )
         }
 
+        function setStarInCenter(numLetters: number, playerCardCells: NodeListOf<HTMLTableDataCellElement>) {
+            const centerCellIndex = 2 * numLetters + Math.floor(numLetters / 2)
+            const centerCell = playerCardCells[centerCellIndex]
+            centerCell.textContent = "★"
+            centerCell.classList.add('star')
+            centerCell.classList.add('received')
+        }
+
         for (let colIndex = 0; colIndex < this.NUMBER_OF_LETTERS; colIndex++) {
             const selectedNums = selectFiveNumbers(colIndex * this.NUMBERS_PER_LETTER + 1)
             for (let rowIndex = 0; rowIndex < this.NUMBER_OF_LETTERS; rowIndex++) {
                 this.playerCardCells[rowIndex * this.NUMBER_OF_LETTERS + colIndex].textContent = selectedNums[rowIndex].toString()
             }
         }
-        const centerCellIndex = 2 * this.NUMBER_OF_LETTERS + Math.floor(this.NUMBER_OF_LETTERS / 2);
-        const centerCell = this.playerCardCells[centerCellIndex];
-        centerCell.textContent = "★"
-        centerCell.classList.add('star')
-        centerCell.classList.add('received')
+
+        setStarInCenter(this.NUMBER_OF_LETTERS, this.playerCardCells)
     }
 
+    /**
+     * Updates the player card with the number given and returns whether the player wins
+     * @param ballNumber the number to mark on the player card
+     */
     private updatePlayerCard(ballNumber: number): boolean {
         this.playerCardCells.forEach(cell => {
             if (cell.textContent === ballNumber.toString()) {
                 cell.classList.add('received')
             }
         })
-        return this.playerWins()
-    }
-
-    private playerWins(): boolean {
-        return this.horizontalWin() || this.verticalWin() || this.diagonalWin()
+        const playerWins = this.horizontalWin() || this.verticalWin() || this.diagonalWin()
+        return playerWins
     }
 
     private horizontalWin(): boolean {
